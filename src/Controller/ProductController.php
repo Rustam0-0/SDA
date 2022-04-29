ad84dacb6e89244e761fc97d4437f1b7ae7e6cdf
rustam <iusupov.rustam1@gmail.com>
2022-04-29 20:25:53 +0200

    initial

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,47 +20,68 @@ class ProductController extends AbstractController
     /**
      * @Route("/", name="app_product_index", methods={"GET"})
      */
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $repocat): Response
     {
+        $categories = $repocat->findAll();
         return $this->render('product/index.html.twig', [
             'products' => $productRepository->findAll(),
+            'categories' => $categories
         ]);
     }
 
     /**
      * @Route("/new", name="app_product_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, ProductRepository $productRepository): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, ProductRepository $productRepository, CategoryRepository $repocat): Response
     {
+        $categories = $repocat->findAll();
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $productRepository->add($product);
+            $product->setDateAdd(new \DateTime());
+            //   dd($request);
+            $aMimeTypes = array("image/gif", "image/jpeg", "image/jpg", "image/png", "image/x-png", "image/tiff");
+            //   $img = ['product']['photo'];
+            $objfichier = $request->files->get('product');
+            $fichier = $objfichier['picture'];
+            if (!empty($fichier)) {
+                if (in_array($fichier->getClientmimeType(), $aMimeTypes)) {
+                    if ($fichier->move('assets/images/PRODUCTS/', $fichier->getClientOriginalName())) {
+                        $product->setPicture($fichier->getClientOriginalName());
+                    }
+                }
+            }
+            $entityManager->persist($product);
+            $entityManager->flush();
+            $this->addFlash('success', 'Produit est ajouté');
             return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('product/new.html.twig', [
             'product' => $product,
             'form' => $form,
+            'categories' => $categories
         ]);
     }
 
     /**
      * @Route("/{id}", name="app_product_show", methods={"GET"})
      */
-    public function show(Product $product): Response
+    public function show(Product $product, CategoryRepository $repocat): Response
     {
+        $categories = $repocat->findAll();
         return $this->render('product/show.html.twig', [
             'product' => $product,
+            'categories' => $categories
         ]);
     }
 
     /**
      * @Route("/{id}/edit", name="app_product_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Product $product, ProductRepository $productRepository): Response
+    public function edit(Request $request, Product $product, ProductRepository $productRepository, CategoryRepository $repocat): Response
     {
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
@@ -68,21 +91,26 @@ class ProductController extends AbstractController
             return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
         }
 
+        $categories = $repocat->findAll();
         return $this->renderForm('product/edit.html.twig', [
             'product' => $product,
             'form' => $form,
+            'categories' => $categories
         ]);
     }
 
     /**
      * @Route("/{id}", name="app_product_delete", methods={"POST"})
      */
-    public function delete(Request $request, Product $product, ProductRepository $productRepository): Response
+    public function delete(Request $request, Product $product, ProductRepository $productRepository, CategoryRepository  $repocat): Response
     {
         if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
             $productRepository->remove($product);
         }
 
-        return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+        $categories = $repocat->findAll();
+        return $this->redirectToRoute('app_product_index', [
+            'categories' => $categories
+        ], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -1,11 +1,50 @@
 <?php
+//
+//namespace App\Form;
+//
+//use App\Entity\Product;
+//use Symfony\Component\Form\AbstractType;
+//use Symfony\Component\Form\FormBuilderInterface;
+//use Symfony\Component\OptionsResolver\OptionsResolver;
+//
+//class ProductType extends AbstractType
+//{
+//    public function buildForm(FormBuilderInterface $builder, array $options): void
+//    {
+//        $builder
+//            ->add('name')
+//            ->add('description')
+//            ->add('picture')
+//            ->add('price')
+//            ->add('stock')
+//            ->add('date_add')
+//            ->add('date_update')
+//            ->add('subcat')
+//        ;
+//    }
+//
+//    public function configureOptions(OptionsResolver $resolver): void
+//    {
+//        $resolver->setDefaults([
+//            'data_class' => Product::class,
+//        ]);
+//    }
+//}
+
 
 namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class ProductType extends AbstractType
 {
@@ -13,14 +52,20 @@ class ProductType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('description')
-            ->add('picture')
-            ->add('price')
+            ->add('subcat', null, ['attr' => ['class' => 'custom-select']])
+            ->add('description', TextareaType::class, ['required' => false])
+            ->add('picture', FileType::class, ['mapped' => false, 'required' => false, 'attr' => ['accept' => 'image/*', 'class' => 'form-control-file'], 'data_class' => null])
+            ->add('price', TextType::class, [
+                'label' => 'Prix',
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex('/[0-9]{1,10}/')
+                ],
+                'attr' => ['class' => 'form-control', 'required' => true]
+            ])
             ->add('stock')
-            ->add('date_add')
-            ->add('date_update')
-            ->add('subcat')
-        ;
+            ->add('date_add', HiddenType::class, ['mapped' => false, 'data_class' => null])
+            ->add('date_update', HiddenType::class, ['mapped' => false, 'data_class' => null]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -30,3 +75,4 @@ class ProductType extends AbstractType
         ]);
     }
 }
+
