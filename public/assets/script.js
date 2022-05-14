8e80d9b7d72103e263f98e649f08e8b37e65f6ec
rustam <iusupov.rustam1@gmail.com>
2022-05-15 00:49:21 +0200

    initial

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,19 +1,19 @@
 // Убавляем кол-во по клику
-$('.quantity_inner .bt_minus').click(function() {
+$('.quantity_inner .bt_minus').click(function () {
     let $input = $(this).parent().find('.quantity');
     let count = parseInt($input.val()) - 1;
     count = count < 1 ? 1 : count;
     $input.val(count);
 });
 // Прибавляем кол-во по клику
-$('.quantity_inner .bt_plus').click(function() {
+$('.quantity_inner .bt_plus').click(function () {
     let $input = $(this).parent().find('.quantity');
     let count = parseInt($input.val()) + 1;
     count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
     $input.val(parseInt(count));
 });
 // Убираем все лишнее и невозможное при изменении поля
-$('.quantity_inner .quantity').bind("change keyup input click", function() {
+$('.quantity_inner .quantity').bind("change keyup input click", function () {
     if (this.value.match(/[^0-9]/g)) {
         this.value = this.value.replace(/[^0-9]/g, '');
     }
@@ -24,3 +24,96 @@ $('.quantity_inner .quantity').bind("change keyup input click", function() {
         this.value = parseInt($(this).data('max-count'));
     }
 });
+
+// Добавляем в корзину
+// $('.quantity_inner .bt_buy').click(function () {
+//
+// });
+
+// $.post(url, {
+//     total_price: total_price,
+//     total_items: total_items
+// }, function(data) { // ожидаем ответа сервера (строка, число или объект)
+//
+//     if(data===1) { // если пришло 1, записываем данные в элементы
+//         $('#bought').html(total_items); // Запись в нужные места на странице
+//         $('#sum').html(total_price);
+//     }
+//     else {
+//         alert('Something's wrong...');
+//     }
+// });
+//
+// $('.bt_buy').click(function () {
+//     let formURL = "{{ path('cart_add')}}";
+//     let formData = new FormData(this);
+//     $.ajax({
+//         url: formURL,
+//         type: 'POST',
+//         data: formData,
+//         mimeType: "multipart/form-data",
+//         contentType: false,
+//         cache: false,
+//         processData: false,
+//         success: function (data, textStatus, jqXHR) {
+//             // console.log($.parseJSON(data));
+//             console.log(data);
+//         },
+//         error: function (jqXHR, textStatus, errorThrown) {
+//             console.log('pjoih');
+//         }
+//     });
+// });
+
+    $('.bt_buy').click(function(){
+        let product_id = $(this).data("productid");
+        let product_name = $(this).data("productname");
+        let product_description = $(this).data("producdescription");
+        let product_picture = $(this).data("producpicture");
+        let product_price = $(this).data("producprice");
+        let quantity = $(this).data("qty");
+        // let quantity = $('#' + product_id).val();
+        // if(quantity != '' && quantity > 0)
+        // {
+        $.ajax({
+            url: url,
+            method:"POST",
+            data:{product_id:product_id, product_name:product_name, product_description:product_description, product_picture:product_picture, product_price:product_price, quantity:quantity},
+            success:function(data)
+            {
+            alert("Product Added into Cart");
+            $('#cart').html(data);
+            $('#' + product_id).val('');
+            }
+        });
+        // }
+        // else
+        // {
+        // alert("Please Enter quantity");
+        // }
+    });
+//
+//     // confirmation de supprision de produit
+//     $(document).on('click', '.button_del', function()
+//     {
+//         // var prod_id = $(this).attr("id");
+//         var prod_id = $('#id').val();
+//         if(confirm("Are you sure you want to remove this?"))
+//         {
+//             $.ajax(
+//             {
+//                 url:"<?php echo base_url(); ?>Pages/product_delete",
+//                 method:"POST",
+//                 data:{prod_id:prod_id},
+//                 success:function(data)
+//                 {
+//                     // alert("Product removed from Cart");
+//                     // $('#cart_details').html(data);
+//                 }
+//             });
+//         }
+//         else
+//         {
+//             return false;
+//         }
+//     });
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -1,20 +1,114 @@
 <?php
+//
+//namespace App\Controller;
+//
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Annotation\Route;
+//
+//class CartController extends AbstractController
+//{
+//    /**
+//     * @Route("/cart", name="app_cart")
+//     */
+//    public function index(): Response
+//    {
+//        return $this->render('cart/index.html.twig', [
+//            'controller_name' => 'CartController',
+//        ]);
+//    }
+//}
+
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Product;
+use App\Repository\CategoryRepositoryRepository;
+use App\Repository\ClientRepositoryRepository;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
 class CartController extends AbstractController
 {
+//    /**
+//     * @Route("/cart/add", name="cart_add")
+//     */
+//    public function cartAdd(SessionInterface $session, ProductRepository $prod, Request $request): Response
+//    {
+//        $cart = $session->get("cart", []);
+//
+//        if ($request->getMethod() == 'POST') {
+//            $product = $prod->find($request->get('id'));
+//            $cart[$request->get('id')] = [
+//                "name" => $request->get('name'),
+//                "description" => $request->get('description'),
+//                "picture" => $request->get('picture'),
+//                "qty" => $request->get('qty'),
+//                "stock" => $product->getStock()
+//            ];
+//            $session->set("cart", $cart);
+//        }
+//        dump($cart);
+//
+//        $referer = $request->headers->get('referer');
+//        return new RedirectResponse($referer);
+//    }
+
     /**
-     * @Route("/cart", name="app_cart")
+     * @Route("/cart/update", name="cart_update")
      */
-    public function index(): Response
+    public function cartUpdate(SessionInterface $session, ProductRepository $prod, Request $request): Response
     {
+        $cart = $session->get("cart", []);
+        if ($request->getMethod() == 'POST') {
+            $product = $prod->find($request->get('id'));
+            $id = $request->get('id');
+
+            if ($request->get('qty') > $product->getStock()) {
+                $this->addFlash('danger', 'Attention le stock disponible!');
+                return $this->redirectToRoute("cart");
+            }
+
+            if ($request->get('qty') <= 0) {
+                unset($cart[$id]);
+            } else {
+                $cart[$id]["qty"] = $request->get('qty');
+            }
+        }
+        $session->set("cart", $cart);
+
+        $referer = $request->headers->get('referer');
+        return new RedirectResponse($referer);
+    }
+
+    /**
+     * @Route("/cart", name="cart")
+     */
+    public function index(CategoryRepository $repocat, SessionInterface $session, ProductRepository $prod): Response
+    {
+        $cart = $session->get("cart", []);
+
+        foreach ($cart as $key => $value) {
+            $product = $prod->find($key);
+            $cart[$key]['price'] = $product->getPrice();
+        }
+
+        dump($cart);
+
+        $categories = $repocat->findAll();
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'categories' => $categories,
+            'cart' => $cart,
         ]);
     }
+
+
 }
