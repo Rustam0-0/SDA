e21f2411c16782a94fad9b2910ada8c886b448f4
rustam <iusupov.rustam1@gmail.com>
2022-05-31 23:43:50 +0200

    initial

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ClientRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -51,6 +53,16 @@ class Client
      * @ORM\Column(type="string", length=10, nullable=true)
      */
     private $tel;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Order::class, mappedBy="client")
+     */
+    private $orders;
+
+    public function __construct()
+    {
+        $this->orders = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
